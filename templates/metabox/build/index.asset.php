d02f034bad8a697ef877866cbb8dc7454e73a0b8
Szymon Mazurczak <biuro@tenbajt.pl>
2022-07-10 22:01:36 +0200

    1.0.0

---
+++ templates/metabox/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '1ca92e9f07ae68337ea1');
---
+++ templates/shortcode/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '0ce96944e4d25f0bf89c');
